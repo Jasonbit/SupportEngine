6f3eb370e9524582c83781958a551b1d8d0651cb
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-08-14 12:41:09 -0300

    tweak engine code

--- lib/support_engine/engine.rb
+++ lib/support_engine/engine.rb
@@ -9,17 +9,17 @@ module SupportEngine
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
       g.assets false
       g.helper false
+    end
 
-      initializer 'support_engine.action_view' do |app|
-        ActiveSupport.on_load :action_view do
-          ::ActionView::Base.send :include, SupportEngine::WidgetHelper
-        end
+    initializer 'support_engine.action_view' do |app|
+      ActiveSupport.on_load :action_view do
+        ::ActionView::Base.send :include, SupportEngine::WidgetHelper
       end
+    end
 
-      initializer :append_migrations do |app|
-        unless app.root.to_s.match root.to_s
-          app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
-        end
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        app.config.paths["db/migrate"] += config.paths["db/migrate"].expanded
       end
     end
   end
