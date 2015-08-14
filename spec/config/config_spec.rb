892a0c41453348d3abbe3c22694e8cca46478d96
Gabriel Rios <gabrielfalcaorios@gmail.com>
2015-08-14 12:40:20 -0300

    Add some tests to improve coverage

--- spec/config/config_spec.rb
+++ spec/config/config_spec.rb
@@ -4,11 +4,30 @@ describe SupportEngine::Configuration do
   subject { SupportEngine.config }
 
   describe 'user_class' do
-    before(:all) do
-      SupportEngine.configure {|c| c.user_class_name = "User" }
+    context "defaul values" do
+      before do
+        SupportEngine.configure do|c|
+          c.user_class_name = "User"
+          c.email_address = nil
+          c.email_prefix = nil
+        end
+      end
+      it { expect(subject.email_prefix).to eq("SupportEngine") }
+      it { expect(subject.email_address).to eq("no-reply@supportengine.org") }
     end
 
-    it { expect(subject.user_class).to eq(User) }
-    it { expect(subject.user_class_name).to eq("User") }
+    context "value" do
+      before do
+        SupportEngine.configure do|c|
+          c.user_class_name = "User"
+          c.email_address = "no-reply@test.com"
+          c.email_prefix = "Test"
+        end
+      end
+      it { expect(subject.user_class).to eq(User) }
+      it { expect(subject.user_class_name).to eq("User") }
+      it { expect(subject.email_address).to eq("no-reply@test.com") }
+      it { expect(subject.email_prefix).to eq("Test") }
+    end
   end
 end
--- spec/mailers/support_engine/ticket_spec.rb
+++ spec/mailers/support_engine/ticket_spec.rb
@@ -5,6 +5,13 @@ module SupportEngine
     let(:type) { double(SupportType, name: "Question", email: "test@supportengine.org") }
     let(:ticket) { double(Ticket, name: "Gabriel", email: "gabriel@text.com", body: "",  title: "Something Wrong", type: type) }
     let(:mail) { TicketMailer.notify(type, ticket) }
+    before do
+      SupportEngine.configure do|c|
+        c.user_class_name = "User"
+        c.email_address = nil
+        c.email_prefix = nil
+      end
+    end
 
     it 'renders the subject' do
       expect(mail.subject).to eql('[SupportEngine][Question] Something Wrong')
